c3a366fa2eeaf2057fd15580b4fb06fc4a8db0cd
Alexthian19 <ge19008@ues.edu.sv>
2023-06-09 12:24:01 -0600

    bugs fixs

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,30 @@
+"use client";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
-export default function Home() {
+const WelcomePage = () => {
   return (
-    <div>
-      HOME
-    </div>
+    <Box
+      maxW="container.sm"
+      m="auto"
+      p={4}
+      display="flex"
+      justifyContent="center"
+    >
+      <Box textAlign="center">
+        <Heading as="h1" mb={4}>
+          Bienvenido al Sistema de Gestión de Empleados
+        </Heading>
+        <Text fontSize="xl" mb={6}>
+          MP Consultoría y Construcción S.A. DE C.V.
+        </Text>
+        <Text fontSize="lg" mb={8}>
+          ¡Gracias por utilizar nuestro sistema! Aquí podrás gestionar la
+          información de los empleados, administrar los registros de asistencia,
+          generar reportes y más.
+        </Text>
+      </Box>
+    </Box>
   );
-}
+};
+
+export default WelcomePage;
