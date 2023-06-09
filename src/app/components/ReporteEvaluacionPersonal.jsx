5ff815a4f815e08f12cb01f41bbceb866319b314
Alexthian19 <ge19008@ues.edu.sv>
2023-06-09 12:54:20 -0600

    margen de evaluacion

--- src/app/components/ReporteEvaluacionPersonal.jsx
+++ src/app/components/ReporteEvaluacionPersonal.jsx
@@ -128,7 +128,7 @@ export default function EvalPersonal() {
   const firma = "Firma";
 
   return (
-    <Box mx="10%" my={3}>
+    <Box mx="10%" my={4}>
       <Box mb={4}>{fechaActual}</Box>
       <Box mb={4}>
         <label htmlFor="empleadoInput">Empleado: </label>
