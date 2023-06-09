d052229f17f1c05631b7194a37ecc899feb44017
Alexthian19 <ge19008@ues.edu.sv>
2023-06-08 20:32:34 -0600

    prestaciones mod

--- src/app/gestion-nominas/nomina-mensual/page.jsx
+++ src/app/gestion-nominas/nomina-mensual/page.jsx
@@ -1,4 +1,8 @@
+"use client"; 
+import { prestacionesCalc } from "@app/utils/prestacionesCalc";
+
 export default function NominaMensual() {
+    console.log(prestacionesCalc(365));
     return (
       <>
         <div>NominaMensual</div>
--- src/app/gestion-nominas/prestaciones/page.jsx
+++ src/app/gestion-nominas/prestaciones/page.jsx
@@ -1,9 +1,7 @@
 'use client';
 import PrestacionesTable from "@app/components/PrestacionesTable";
-import { prestacionesCalc } from "@app/utils/prestacionesCalc";
 
 export default function Prestaciones() {
-    console.log(prestacionesCalc(365));
     return (
       <>
         < PrestacionesTable />
